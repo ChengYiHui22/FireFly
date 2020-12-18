caa38e36b367d0557560742c8d92ad7dbb986e41
Chengyihui22 <602304432@qq.com>
2020-12-18 08:42:12 +0800

    提交呀main

--- FireFly/pages/First/first.js
+++ FireFly/pages/First/first.js
@@ -1,18 +1,62 @@
 // pages/First/first.js
+import fetch from "../../fetch"
+import setinfo from "../../setinfo"
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        // 轮播图片
+        imgarray:{},
+        best:[],
+        newest:[],
+        top:0,
+        scrollTop:0
+            
     },
+    goTo(e){
+        wx.navigateTo({
+          url: '../goods/goods?goos_id='+e.detail.goods,
+          success: (result) => {},
+          fail: (res) => {},
+          complete: (res) => {},
+        })
+    },
+    //监听界面滚动
+    onPageScroll (e) { 
+        if(e.scrollTop>300){
+            this.setData({
+                top:1
+            })
+        }else{
+            this.setData({
+                top:0,
+            })
+        }
+        },
+        //回到顶部
+        goTop(e){
+        wx.pageScrollTo({
+          duration: 0,
+          scrollTop:0,
+        })
+        },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        fetch({url:setinfo.siteroot+setinfo.sitefirst+'index/page'+setinfo.sitelast,method:'GET'}).then(data=>{
+            console.log(data.data.data)
+            this.setData({
+                imgarray:data.data.data.items.n606196612728596.data,
+                newest:data.data.data.newest,
+                best:data.data.data.best
+            })
+        }).catch(error=>{
+            console.log(error)
+        })
     },
 
     /**
